b0518918643dca874eaee1b85a5f790d716638de
jhamilton <josehamiltonsantosjunior@gmail.com>
2021-12-08 21:38:39 -0300

    all files and directory frontend removed

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 import 'dotenv/config';
 import express, { json } from 'express';
-import path from 'path';
 
 import router from './interfaces/routes/index.js';
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'));
-
 app.use(json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, '..', 'static')));
 
 router(app);
 
