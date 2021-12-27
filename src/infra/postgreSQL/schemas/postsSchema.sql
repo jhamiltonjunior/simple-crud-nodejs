5cd3c99b77826d67f6cac5bf7bf317a092ea5b56
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2021-12-27 18:58:20 -0300

    kde > gnome

--- src/infra/postgreSQL/schemas/postsSchema.sql
+++ src/infra/postgreSQL/schemas/postsSchema.sql
@@ -1,5 +1,23 @@
 CREATE DATABASE nauts;
 
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(30) UNIQUE NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  updated_at TIMESTAMP DEFAULT NOW()
+)
+
+INSERT INTO users (username, name, email, password)
+VALUES ('jhsj', 'Hamilton', 'hamilton@gmail.com', '12345');
+
+INSERT INTO users (username, name, email, password)
+VALUES ('jose', 'José', 'jose@gmail.com', '123456');
+
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
@@ -7,7 +25,7 @@ CREATE TABLE posts (
   title TEXT NOT NULL,
   body TEXT NOT NULL,
   url_params VARCHAR(60) UNIQUE NOT NULL,
-  author VARCHAR(30) NOT NULL,
+  author_id VARCHAR(30) NOT NULL,
   created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   updated_at TIMESTAMP DEFAULT NOW()
 );
