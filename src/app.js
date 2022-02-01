b9162d0c2c35c6d89cc9059b0a4727b10a0a83f6
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-02-01 19:31:04 -0300

    liberando acesso externo a essa api

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(json({ type: 'application/vnd.api+json' }));
 
 app.use((_, res, next) => {
+  // All access
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 
